61abb3d097f7816533b495eda30191155e8d8a4e
JMonserrat1 <monserratjere@gmail.com>
2025-08-11 20:46:29 -0300

    primer subida del proyecto

---
+++ src/main/java/com/flujos/Entidades/ClienteProveedor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.flujos.Entidades;
+
+/**
+ *
+ * @author monse
+ */
+public class ClienteProveedor {
+    
+    private Long idClienteProveedor;
+    private String nomRazonSocial;
+    private Integer dniCuit;
+    private String telefono;
+    private String email;
+    private String tipoClienteProveedor;    
+}
---
+++ src/main/java/com/flujos/flujo_fondos/Flujo_Fondos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.flujos.flujo_fondos;
+
+/**
+ *
+ * @author monse
+ */
+public class Flujo_Fondos {
+
+    public static void main(String[] args) {
+        
+    }
+}
