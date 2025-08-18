26081f0ddc48bc4b963a499cf6d2c29f0e67183b
JMonserrat1 <monserratjere@gmail.com>
2025-08-18 20:04:14 -0300

    Subida entidad ClienteProveedor

--- src/main/java/com/flujos/Entidades/ClienteProveedor.java
+++ src/main/java/com/flujos/Entidades/ClienteProveedor.java
@@ -15,5 +15,69 @@ public class ClienteProveedor {
     private Integer dniCuit;
     private String telefono;
     private String email;
-    private String tipoClienteProveedor;    
+    private String tipoClienteProveedor;
+    
+    
+    public ClienteProveedor(Long idClienteProveedor, String nomRazonSocial, Integer dniCuit, String telefono, String email, String tipoClienteProveedor) {
+        this.idClienteProveedor = idClienteProveedor;
+        this.nomRazonSocial = nomRazonSocial;
+        this.dniCuit = dniCuit;
+        this.telefono = telefono;
+        this.email = email;
+        this.tipoClienteProveedor = tipoClienteProveedor;
+    }
+
+    public ClienteProveedor() {
+        
+    }
+
+    public Long getIdClienteProveedor() {
+        return idClienteProveedor;
+    }
+
+    public void setIdClienteProveedor(Long idClienteProveedor) {
+        this.idClienteProveedor = idClienteProveedor;
+    }
+
+    public String getNomRazonSocial() {
+        return nomRazonSocial;
+    }
+
+    public void setNomRazonSocial(String nomRazonSocial) {
+        this.nomRazonSocial = nomRazonSocial;
+    }
+
+    public Integer getDniCuit() {
+        return dniCuit;
+    }
+
+    public void setDniCuit(Integer dniCuit) {
+        this.dniCuit = dniCuit;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipoClienteProveedor() {
+        return tipoClienteProveedor;
+    }
+
+    public void setTipoClienteProveedor(String tipoClienteProveedor) {
+        this.tipoClienteProveedor = tipoClienteProveedor;
+    }   
+    
+    
 }
