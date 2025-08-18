e28b765ba245aaee751975ac944b72cb29f84bb4
Sol <Sol@DESKTOP-OEK2EV5>
2025-08-18 20:08:20 -0300

    subida de las entidades cuenta y flujosmov

--- src/main/java/com/flujos/Entidades/Cuenta.java
+++ src/main/java/com/flujos/Entidades/Cuenta.java
@@ -10,4 +10,72 @@ package com.flujos.Entidades;
  */
 public class Cuenta {
     
+    private Long idCuenta;
+    private String codConcepto;
+    private String nombreConcepto;
+    private String claseConcepto;
+    private Long idMovimiento;
+    private String Ingreso;
+
+    public Cuenta(Long idCuenta, String codConcepto, String nombreConcepto, String claseConcepto, Long idMovimiento, String Ingreso) {
+        this.idCuenta = idCuenta;
+        this.codConcepto = codConcepto;
+        this.nombreConcepto = nombreConcepto;
+        this.claseConcepto = claseConcepto;
+        this.idMovimiento = idMovimiento;
+        this.Ingreso = Ingreso;
+    }
+
+    public Cuenta() {
+    }
+
+    public Long getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(Long idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+
+    public String getCodConcepto() {
+        return codConcepto;
+    }
+
+    public void setCodConcepto(String codConcepto) {
+        this.codConcepto = codConcepto;
+    }
+
+    public String getNombreConcepto() {
+        return nombreConcepto;
+    }
+
+    public void setNombreConcepto(String nombreConcepto) {
+        this.nombreConcepto = nombreConcepto;
+    }
+
+    public String getClaseConcepto() {
+        return claseConcepto;
+    }
+
+    public void setClaseConcepto(String claseConcepto) {
+        this.claseConcepto = claseConcepto;
+    }
+
+    public Long getIdMovimiento() {
+        return idMovimiento;
+    }
+
+    public void setIdMovimiento(Long idMovimiento) {
+        this.idMovimiento = idMovimiento;
+    }
+
+    public String getIngreso() {
+        return Ingreso;
+    }
+
+    public void setIngreso(String Ingreso) {
+        this.Ingreso = Ingreso;
+    }
+    
+    
 }
--- src/main/java/com/flujos/Entidades/FlujosMov.java
+++ src/main/java/com/flujos/Entidades/FlujosMov.java
@@ -4,10 +4,93 @@
  */
 package com.flujos.Entidades;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  *
  * @author monse
  */
 public class FlujosMov {
+    
+    private Long idFlujoMovimineto;
+    private Date fechaMovimiento;
+    private Long idMovimiento;
+    private Long idCuenta;
+    private BigDecimal importe;
+    private String observacionesMovimiento;
+    private Long idCheque;
+
+    
+    public FlujosMov(Long idFlujoMovimineto, Date fechaMovimiento, Long idMovimiento, Long idCuenta, BigDecimal importe, String observacionesMovimiento, Long idCheque) {
+        this.idFlujoMovimineto = idFlujoMovimineto;
+        this.fechaMovimiento = fechaMovimiento;
+        this.idMovimiento = idMovimiento;
+        this.idCuenta = idCuenta;
+        this.importe = importe;
+        this.observacionesMovimiento = observacionesMovimiento;
+        this.idCheque = idCheque;
+    }
+
+    public FlujosMov() {
+    }
+
+    public Long getIdFlujoMovimineto() {
+        return idFlujoMovimineto;
+    }
+
+    public void setIdFlujoMovimineto(Long idFlujoMovimineto) {
+        this.idFlujoMovimineto = idFlujoMovimineto;
+    }
+
+    public Date getFechaMovimiento() {
+        return fechaMovimiento;
+    }
+
+    public void setFechaMovimiento(Date fechaMovimiento) {
+        this.fechaMovimiento = fechaMovimiento;
+    }
+
+    public Long getIdMovimiento() {
+        return idMovimiento;
+    }
+
+    public void setIdMovimiento(Long idMovimiento) {
+        this.idMovimiento = idMovimiento;
+    }
+
+    public Long getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(Long idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+
+    public BigDecimal getImporte() {
+        return importe;
+    }
+
+    public void setImporte(BigDecimal importe) {
+        this.importe = importe;
+    }
+
+    public String getObservacionesMovimiento() {
+        return observacionesMovimiento;
+    }
+
+    public void setObservacionesMovimiento(String observacionesMovimiento) {
+        this.observacionesMovimiento = observacionesMovimiento;
+    }
+
+    public Long getIdCheque() {
+        return idCheque;
+    }
+
+    public void setIdCheque(Long idCheque) {
+        this.idCheque = idCheque;
+    }
+    
+    
     
 }
